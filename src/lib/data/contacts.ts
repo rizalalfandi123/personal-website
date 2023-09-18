903e5598c600164503767bb343e20f1d4f9f5b90
Rizal Alfandi <rizalalfandi123456789@gmail.com>
2023-09-18 22:31:47 +0700

    add upwork link

--- src/lib/data/contacts.ts
+++ src/lib/data/contacts.ts
@@ -4,10 +4,12 @@ import GithubIcon from '$lib/components/icons/github.icon.svelte';
 import LinkedInIcon from '$lib/components/icons/linkedin.icon.svelte';
 import TelegramIcon from '$lib/components/icons/telegram.icon.svelte';
 import MailIcon from '$lib/components/icons/mail.icon.svelte';
+import UpworkIcon from '$lib/components/icons/upwork.icon.svelte';
 
 export const contacts: Array<Contect> = [
 	{ link: 'https://github.com/rizalalfandi123/', platform: 'Github', icon: GithubIcon },
 	{ link: 'https://www.linkedin.com/in/rizal-alfandi/', platform: 'Linked In', icon: LinkedInIcon },
 	{ link: 'https://telegram.me/RizalAlfandi', platform: 'Instagram', icon: TelegramIcon },
-	{ link: 'mailto:rizalalfandi123456789@gmail.com"', platform: 'Email', icon: MailIcon }
+	{ link: 'mailto:rizalalfandi123456789@gmail.com"', platform: 'Email', icon: MailIcon },
+	{ link: 'https://www.upwork.com/freelancers/~0184b429844e7f9abd', platform: 'Upwork', icon: UpworkIcon }
 ];
